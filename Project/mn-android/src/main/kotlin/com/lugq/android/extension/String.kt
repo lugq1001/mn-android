dc168174b7e3ceb97ebb201ba54d6a5d447dbe15
DESKTOP-I6964RC\lugq1 <lugq1001@icloud.com>
2018-10-17 14:35:47 +0800

    no message

--- Project/mn-android/src/main/kotlin/com/lugq/android/extension/String.kt
+++ Project/mn-android/src/main/kotlin/com/lugq/android/extension/String.kt
@@ -1,6 +1,8 @@
 package com.lugq.android.extension
 
 import android.text.TextUtils
+import java.math.BigInteger
+import java.security.MessageDigest
 import java.util.regex.Pattern
 
 fun String.isIp(): Boolean {
@@ -11,4 +13,23 @@ fun String.isIp(): Boolean {
     val pattern = Pattern.compile(rexEx)
     val matcher = pattern.matcher(this)
     return matcher.matches()
+}
+
+fun String.md5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+}
+
+
+fun String.getUrlParameter(url: String, name: String): String? {
+    var result: String? = null
+    val index = url.indexOf("?")
+    val temp = url.substring(index + 1)
+    val keyValue = temp.split("&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    for (str in keyValue) {
+        if (str.startsWith(name)) {
+            result = str.replace("$name=", "")
+        }
+    }
+    return result
 }
