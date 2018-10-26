79e0ae30606a2ecb1ca64333e449871db26c2e55
陆广庆 <lugq1001@icloud.com>
2018-10-26 18:00:50 +0800

    no message

--- Project/mn-android/src/main/kotlin/com/lugq/android/extension/String.kt
+++ Project/mn-android/src/main/kotlin/com/lugq/android/extension/String.kt
@@ -7,21 +7,22 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import java.util.regex.Pattern
 
-fun String.isIp(): Boolean {
-    if (TextUtils.isEmpty(this)) {
-        return false
+val String.isIPv4: Boolean
+    get() {
+        if (TextUtils.isEmpty(this)) {
+            return false
+        }
+        val rexEx = """(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]).(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]).(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])"""
+        val pattern = Pattern.compile(rexEx)
+        val matcher = pattern.matcher(this)
+        return matcher.matches()
     }
-    val rexEx = """(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]).(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]).(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])"""
-    val pattern = Pattern.compile(rexEx)
-    val matcher = pattern.matcher(this)
-    return matcher.matches()
-}
 
-fun String.md5(): String {
-    val md = MessageDigest.getInstance("MD5")
-    return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
-}
-
+val String.md5: String
+    get() {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+    }
 
 fun String.getQueryParameter(name: String): String? {
     try {
