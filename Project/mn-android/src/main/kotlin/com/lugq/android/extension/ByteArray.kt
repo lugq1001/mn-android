40c813a0f45f8294c46e0347ffaa11988c4d4dc0
DESKTOP-I6964RC\lugq1 <lugq1001@icloud.com>
2018-11-07 00:22:28 +0800

    no message

---
+++ Project/mn-android/src/main/kotlin/com/lugq/android/extension/ByteArray.kt
@@ -0,0 +1,55 @@
+package com.lugq.android.extension
+
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+private val HEX_CHARS = "0123456789ABCDEF".toCharArray()
+
+fun ByteArray.toHexString(): String {
+    val result = StringBuffer()
+    forEach {
+        val octet = it.toInt()
+        val firstIndex = (octet and 0xF0).ushr(4)
+        val secondIndex = octet and 0x0F
+        result.append(HEX_CHARS[firstIndex])
+        result.append(HEX_CHARS[secondIndex])
+    }
+    return result.toString()
+}
+
+fun ByteArray.read(pos: Int, length: Int): ByteArray {
+    val newBytes = ByteArray(length)
+    for ((j, i) in (pos until size).withIndex()) {
+        val b = this[i]
+        newBytes[j] = b
+    }
+    return newBytes
+}
+
+object ByteUtils {
+
+    fun merger(vararg values: ByteArray): ByteArray {
+        var lengthByte = 0
+        for (value in values) {
+            lengthByte += value.size
+        }
+        val allByte = ByteArray(lengthByte)
+        var countLength = 0
+        for (b in values) {
+            System.arraycopy(b, 0, allByte, countLength, b.size)
+            countLength += b.size
+        }
+        return allByte
+    }
+
+    // 高位在前，低位在后
+    fun int2bytes(int: Int): ByteArray {
+        val b = ByteBuffer.allocate(4)
+        b.order(ByteOrder.LITTLE_ENDIAN)
+        b.putInt(int)
+        return b.array()
+    }
+
+
+}
+
--- Project/mn-android/src/main/kotlin/com/lugq/android/extension/String.kt
+++ Project/mn-android/src/main/kotlin/com/lugq/android/extension/String.kt
@@ -7,6 +7,19 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import java.util.regex.Pattern
 
+private const val HEX_CHARS = "0123456789ABCDEF"
+
+fun String.hexToByteArray(): ByteArray {
+    val result = ByteArray(length / 2)
+    for (i in 0 until length step 2) {
+        val firstIndex = HEX_CHARS.indexOf(this[i])
+        val secondIndex = HEX_CHARS.indexOf(this[i + 1])
+        val octet = firstIndex.shl(4).or(secondIndex)
+        result[i.shr(1)] = octet.toByte()
+    }
+    return result
+}
+
 val String.isIPv4: Boolean
     get() {
         if (TextUtils.isEmpty(this)) {
