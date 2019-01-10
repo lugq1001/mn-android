2b5c1a70cb105848b511d817b52563531512a90f
陆广庆 <lugq1001@icloud.com>
2019-01-10 18:48:51 +0800

    no message

--- Project/mn-android/src/main/kotlin/com/lugq/android/util/AppUtil.kt
+++ Project/mn-android/src/main/kotlin/com/lugq/android/util/AppUtil.kt
@@ -1,3 +1,22 @@
 package com.lugq.android.util
 
-object AppUtil
+import android.app.ActivityManager
+import android.content.Context
+
+object AppUtil {
+
+
+    fun isAppOnForeground(context: Context): Boolean {
+        val activityManager = (context.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager)
+                ?: return false
+        val packageName = context.packageName
+        val appProcesses = activityManager.runningAppProcesses ?: return false
+        for (appProcess in appProcesses) {
+            if (appProcess.processName == packageName && appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return true
+            }
+        }
+        return false
+    }
+
+}
